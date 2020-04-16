f2650aab18684d957d805edd236668b2c9df8353
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-04-16 21:58:46 +0900

    Display html with ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,48 @@
 $(function(){
+
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="message-wrap">
+                    <div class="message-wrap__info">
+                      <div class="message-wrap__info--name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message-wrap__info--date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-wrap__body">
+                      <p class="message-wrap__body--text">
+                        ${message.body}
+                      </p>
+                    <img class="message-wrap__body--image" src=${message.image}>
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="message-wrap">
+                    <div class="message-wrap__info">
+                      <div class="message-wrap__info--name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message-wrap__info--date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-wrap__body">
+                      <p class="message-wrap__body--text">
+                        ${message.body}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
-    let url = $(this).attr('action');
+    let url = $(this).attr('action')
     $.ajax({
       url: url,
       type: 'POST',
@@ -10,6 +50,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.chat-space__view').append(html);
+      $('form')[0].reset();
+      $('.chat-space__view').animate({ scrollTop: $('.chat-space__view')[0].scrollHeight});
+      $('.form-submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
     });
-  });
+  })
 });
