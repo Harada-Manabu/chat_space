6f622f4de007f50755b0218dc347ec960b15e178
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-04-17 19:08:33 +0900

    Add member in the list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  function appendUser(user) {
+  function addUser(user) {
     let html = `
     <div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${user.name}</p>
@@ -19,6 +19,20 @@ $(function(){
   $("#user-search-result").append(html);
   }
 
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
 
   $('#user-search-field').on('keyup', function() {
     let input = $('#user-search-field').val();
@@ -32,7 +46,7 @@ $(function(){
       $('#user-search-result').empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
-          appendUser(user);
+          addUser(user);
         });
       } else if (input.length == 0) {
         return false;
@@ -44,4 +58,18 @@ $(function(){
       alert('通信エラーです。ユーザーが表示できません。');
     });
   });
+
+  $(document).on('click', '.chat-group-user__btn--add', function(){
+    const userName = $(this).attr('data-user-name');
+    const userId = $(this).attr('data-user-id');
+    $(this).parent().remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on('click', '.chat-group-user__btn--remove', function() {
+    $(this).parent().remove();
+  })
+  $('edit-btn').on('click', function(){
+
+  })
 });
