52393eb7de93df5b4e2e758bb0af928edc3e5e9f
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-04-17 15:50:44 +0900

    Create json data to html

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,25 @@
 $(function(){
 
+  function appendUser(user) {
+    let html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    </div>
+    `;
+    $('#user-search-result').append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">ユーザーが見つかりません</p>
+    </div>
+  `;
+  $("#user-search-result").append(html);
+  }
+
+
   $('#user-search-field').on('keyup', function() {
     let input = $('#user-search-field').val();
     $.ajax({
@@ -8,11 +28,20 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
-    .done(function(){
-      console.log('its working');
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+         addNoUser();
+      }
     })
     .fail(function(){
-      console.log('doesnt work');
+      alert('通信エラーです。ユーザーが表示できません。');
     });
   });
 });
